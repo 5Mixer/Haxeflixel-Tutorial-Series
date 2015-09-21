bb1ab13eda0e7ba294ac08bbf9fda29028befef3
Mixerman123 <dantheman41@live.com.au>
2015-09-21 13:55:43 +1000

    Planet defence Ep 3

--- Planet Defence/source/Player.hx
+++ Planet Defence/source/Player.hx
@@ -4,20 +4,30 @@ import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.util.FlxAngle;
 import flixel.FlxG;
+import flixel.FlxObject;
 import Planet;
 
 class Player extends FlxSprite {
 
 	var angleFromPlanet:Float;
 	var planet:Planet;
+	var speed:Float = 1;
 
 	public function new (_angle,_planet){
 		super(0,0);
 
+		setFacingFlip(FlxObject.RIGHT,false,false);
+		setFacingFlip(FlxObject.LEFT,true,false);
+
 		angleFromPlanet = _angle;
 		planet = _planet;
 
-		makeGraphic(16,16,FlxColor.GREEN);
+		//makeGraphic(16,16,FlxColor.GREEN);
+		loadGraphic("assets/images/Player.png",true,16,16);
+
+		animation.add("Idle",[0,1],2,false);
+		animation.add("Walk",[6,7,8,9],15,false);
+
 
 		FlxG.camera.follow(this);
 	}
@@ -25,7 +35,24 @@ class Player extends FlxSprite {
 	override public function update () {
 		super.update();
 
-		angleFromPlanet+= FlxG.elapsed;
+		var leftKey = FlxG.keys.anyPressed(["LEFT","A"]);
+		var rightKey = FlxG.keys.anyPressed(["RIGHT","D"]);
+		var shootKey = FlxG.keys.anyPressed(["UP","W","SPACE"]);
+
+		if (leftKey){
+			angleFromPlanet -= FlxG.elapsed * speed;
+			animation.play("Walk");
+		}else if (rightKey){
+			angleFromPlanet+= FlxG.elapsed * speed;
+			animation.play("Walk");
+		}else{
+			animation.play("Idle");
+		}
+
+		if (shootKey){
+			//The player should shoot
+		}
+		
 
 		calculatePosition();
 	}
@@ -35,7 +62,7 @@ class Player extends FlxSprite {
 		//It also rotates the sprite to face the planet
 		x = Math.cos(angleFromPlanet-90 * Math.PI / 180) * (planet.planetRadius - 4 + 8) + planet.getMidpoint().x - 8;
 		y = Math.sin(angleFromPlanet-90 * Math.PI / 180) * (planet.planetRadius - 4  + 8) + planet.getMidpoint().y - 8;
-
+		
 		angle = FlxAngle.angleBetween(planet,this,true)+90;
 	}
 }
